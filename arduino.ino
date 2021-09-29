3533ddb79968b319a74f5d06fa11df159491ce6d
Gokulraj-R-002 <gokulraj18921@gmail.com>
2021-09-30 00:07:17 +0530

    updated the codes

--- arduino.ino
+++ arduino.ino
@@ -4,32 +4,28 @@
 SoftwareSerial serial_info_from_nodemcu(3, 2); // Rx and Tx pins
 
 const int stepPerRev = 200;
-Stepper obj_stepper(stepPerRev, 13, 12, 11, 10);
-Stepper scr_stepper(stepPerRev, 9, 8, 7, 6);
-
+Stepper mystepper(stepPerRev, 13, 12, 11, 10); // pins 10, 11, 12 and 13 are used to control the motor
+String steps;
+char To_move;
+int steps1;
 void setup() {
   Serial.begin(9600);
   serial_info_from_nodemcu.begin(4800);
-  
-  obj_stepper.setSpeed(5);
-  scr_stepper.setSpeed(5);
-
+  mystepper.setSpeed(20);
 }
 
 void loop() {
   if(serial_info_from_nodemcu.available() > 0) {
     String str = serial_info_from_nodemcu.readStringUntil('\n');
-//    Serial.println(str);
-
-    steps = str.substring(1);
-    
-    if(str[0] == 'O') {
-      obj_stepper.step(steps.toInt());
-      Serial.println("moved -5");
+   Serial.println(str);
+   To_move = str[0];
+  steps = str.substring(1);
+  steps1 = steps.toInt();
+    if(To_move == 'O') {
+      mystepper.step(steps1);
     }
-    else if (str[0] == 'I') {
-      scr_stepper.step(steps.toInt());
-      Serial.println("moved 5");
+    else if (To_move == 'S') {
+      mystepper.step(steps1);
     }
   }
 }
--- nodemcu.ino
+++ nodemcu.ino
@@ -1,12 +1,10 @@
-/* Comment this out to disable prints and save space */
-#define BLYNK_PRINT Serial
-
-/* Fill-in your Template ID (only if using Blynk.Cloud) */
-//#define BLYNK_TEMPLATE_ID   "YourTemplateID"
-
-#include <SoftwareSerial.h>
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
+#include <SoftwareSerial.h>
+
+#define BLYNK_PRINT Serial
+
+SoftwareSerial serial_info_to_arduino(D2, D3); // Rx and Tx pins
 
 // You should get Auth Token in the Blynk App.
 // Go to the Project Settings (nut icon).
@@ -14,72 +12,132 @@ char auth[] = "arbTMNFpk18iGtb0e2SmJosJLJ41uagS";
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
-char ssid[] = "";
-char pass[] = "";
+char ssid[] = "RISMIT_770";
+char pass[] = "RISHUmitu*8978@770";
+
+const int stepsPerRevolution = 200; // change this to fit the number of steps per revolution
+// for your motor
+const float distPerstep = 1; //(in cm)
+const int motor_spped = 20;
+
+int Recall = 0;
+int val = 0;
+bool obj_recal = false;
+bool scrn_recal = true;
 
 
-float curr_obj_pos = 0, curr_scr_pos = 0;
-float new_obj_pos, new_scr_pos;
+int obj_endpin1 = 0, obj_endpin2 = 0, scrn_endpin1 = 0,scrn_endpin2 = 0;
 
-int recalibrate = 0;
+float obj_dist;
+float scrn_dist;
 
-void Recalibrate() {
-  
+float prev_obj = 0;
+float prev_scrn = 0;
+int obj_StepsToTake = 0, scrn_StepsToTake = 0, count = 0 ;
+int Timetorotate;
+
+
+float obj_Recallibrate(float obj_dist) {
+  obj_dist = 0;
+  Blynk.virtualWrite(2, 0);
+
+  count = 0;
+
+  while(obj_endpin1!=1){
+    serial_info_to_arduino.println("O"+String(-1)); // missing
+
+    if(count == 10){
+      obj_endpin1 = 1;
+    }
+    else{
+      count += 1;
+    }
+
+  }
+  return obj_dist;
 }
 
-SoftwareSerial serial_info_to_arduino(D2, D3); // Rx and Tx pins
+float scrn_Recallibrate(float scrn_dist) {
+  scrn_dist = 0;
+  Blynk.virtualWrite(3, 0);
 
-BLYNK_WRITE(V0) {
-  new_obj_pos = param.asFloat();
+  count = 0;
+  Serial.println(scrn_endpin1);
+  Serial.println('a');
+  scrn_endpin1 = digitalRead(D1);
+  Serial.println(scrn_endpin1);
+  Serial.println('b');
+  while(scrn_endpin1 != 1){
+    scrn_endpin1 = digitalRead(D1);
+    Serial.println('c');
+    Serial.println(scrn_endpin1);
+    serial_info_to_arduino.println("S"+ String(-1)); // missing
+
+  }
+  return scrn_dist;
 }
 
-BLYNK_WRITE(V1) {
-  new_scr_pos = param.asFloat();
-}
+void setup(){
 
-BLYNK_WRITE(V2) {
-  recalibrate = param.asInt();
-}
+  Blynk.begin(auth, ssid, pass); // missing
 
-void setup() {
+  // initialize the serial port:
   Serial.begin(9600);
-  Blynk.begin(auth, ssid, pass);
-
   serial_info_to_arduino.begin(4800);
-  pinMode(D2, INPUT);
-  pinMode(D3, OUTPUT);
+
+  pinMode(D1, INPUT);
+
+  //obj_dist = obj_Recallibrate(obj_dist);
+  scrn_dist = scrn_Recallibrate(scrn_dist);
+
+  //serial_info_to_arduino.println("O"+ String(+2));
+  //serial_info_to_arduino.println("S"+ String(+2));
 }
+
+BLYNK_WRITE(V2){
+  obj_dist = param.asFloat();
+}
+BLYNK_WRITE(V3){
+  scrn_dist = param.asFloat();
+}
+BLYNK_WRITE(V4){
+  Recall = param.asInt();
+}
+
+
+// Count pin is just for testing without the setup
 
 void loop() {
+  count = 0;
   Blynk.run();
 
-  if (recalibrate == 1) {
-    Recalibrate();
+  if(Recall == 1){
+    Recall = 0;
+    prev_obj =0;
+    prev_scrn = 0;
+
+    //obj_dist = obj_Recallibrate(obj_dist);
+    scrn_dist = scrn_Recallibrate(scrn_dist);
+
+    Blynk.virtualWrite(4, 0);
+
+    //serial_info_to_arduino.println("O"+ String(+2));
+    //serial_info_to_arduino.println("S"+ String(+2));
   }
 
-  // update the object position
-  if (new_obj_pos == curr_obj_pos) {
-    serial_info_to_arduino.println("O+0");
-  }
-  else if (new_obj_pos < curr_obj_pos) {
-    serial_info_to_arduino.println("O-1");
-    curr_pos = new_pos;
-  }
-  else if (new_obj_pos > curr_scr_pos) {
-    serial_info_to_arduino.println("O+1");
-    curr_pos = new_pos;
-  }
+  obj_StepsToTake = (obj_dist-prev_obj)/distPerstep;
 
-  // update the screen position
-  if (new_scr_pos == curr_scr_pos) {
-    serial_info_to_arduino.println("I+0");
-  }
-  else if (new_scr_pos < curr_scr_pos) {
-    serial_info_to_arduino.println("I-1");
-    curr_scr_pos = new_scr_pos;
-  }
-  else if (new_scr_pos > curr_scr_pos) {
-    serial_info_to_arduino.println("I+1");
-    curr_scr_pos = new_scr_pos;
-  }
+  //for object
+  scrn_StepsToTake = (scrn_dist-prev_scrn)/distPerstep;
+
+  serial_info_to_arduino.println("O"+ String(obj_StepsToTake));
+  serial_info_to_arduino.println("S"+String(scrn_StepsToTake));
+
+  prev_obj = obj_dist;
+  prev_scrn = scrn_dist;
+
+
+
+
+  //delay(Timetorotate);
 }
