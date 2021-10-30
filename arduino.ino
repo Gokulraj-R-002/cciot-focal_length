19439b0002bc4ff6906eaf206512e468c7096ee2
Gokulraj-R-002 <gokulraj18921@gmail.com>
2021-10-30 14:49:01 +0530

    updated the codes

--- arduino.ino
+++ arduino.ino
@@ -4,28 +4,36 @@
 SoftwareSerial serial_info_from_nodemcu(3, 2); // Rx and Tx pins
 
 const int stepPerRev = 200;
-Stepper mystepper(stepPerRev, 13, 12, 11, 10); // pins 10, 11, 12 and 13 are used to control the motor
+Stepper obj_stepper(stepPerRev, 13, 12, 11, 10); // pins 10, 11, 12 and 13 are used to control the motor
+Stepper scr_stepper(stepPerRev, 4, 5, 6, 7);
+
+const int motor_speed = 22;
 String steps;
 char To_move;
 int steps1;
+
 void setup() {
   Serial.begin(9600);
   serial_info_from_nodemcu.begin(4800);
-  mystepper.setSpeed(20);
+  obj_stepper.setSpeed(motor_speed);
+  scr_stepper.setSpeed(motor_speed);
 }
 
 void loop() {
   if(serial_info_from_nodemcu.available() > 0) {
     String str = serial_info_from_nodemcu.readStringUntil('\n');
-   Serial.println(str);
-   To_move = str[0];
-  steps = str.substring(1);
-  steps1 = steps.toInt();
+    
+    Serial.println(str);
+    
+    To_move = str[0];
+    steps = str.substring(1);
+    steps1 = steps.toInt();
+    
     if(To_move == 'O') {
-      mystepper.step(steps1);
+      obj_stepper.step(steps1);
     }
     else if (To_move == 'S') {
-      mystepper.step(steps1);
-    }
+      scr_stepper.step(steps1);
+    }    
   }
 }
--- nodemcu.ino
+++ nodemcu.ino
@@ -12,28 +12,27 @@ char auth[] = "arbTMNFpk18iGtb0e2SmJosJLJ41uagS";
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
-char ssid[] = "RISMIT_770";
-char pass[] = "RISHUmitu*8978@770";
+char ssid[] = "Galaxy M31EA2F";
+char pass[] = "12344321";
 
-const int stepsPerRevolution = 200; // change this to fit the number of steps per revolution
-// for your motor
-const float distPerstep = 1; //(in cm)
+const int stepsPerRevolution = 200; // change this to fit the number of steps per revolution for your motor
+const float distPerstep = 0.1; //(in cm)
 const int motor_spped = 20;
 
 int Recall = 0;
-int val = 0;
 bool obj_recal = false;
-bool scrn_recal = true;
+bool scr_recal = true;
 
 
-int obj_endpin1 = 0, obj_endpin2 = 0, scrn_endpin1 = 0,scrn_endpin2 = 0;
+int obj_endpin1 = 16, obj_endpin2 = 5, scr_endpin1 = 2, scr_endpin2 = 13;
+int obj_end1 = 0, obj_end2 = 0, scr_end1 = 0, scr_end2 = 0;
 
 float obj_dist;
-float scrn_dist;
+float scr_dist;
 
 float prev_obj = 0;
-float prev_scrn = 0;
-int obj_StepsToTake = 0, scrn_StepsToTake = 0, count = 0 ;
+float prev_scr = 0;
+int obj_StepsToTake = 0, scr_StepsToTake = 0 ;
 int Timetorotate;
 
 
@@ -41,83 +40,89 @@ float obj_Recallibrate(float obj_dist) {
   obj_dist = 0;
   Blynk.virtualWrite(2, 0);
 
-  count = 0;
+  obj_end1 = digitalRead(obj_endpin1);
+  
+  while(obj_end1 != 1) {
+    Serial.print(obj_endpin1);
+    Serial.println(obj_end1);
 
-  while(obj_endpin1!=1){
-    serial_info_to_arduino.println("O"+String(-1)); // missing
-
-    if(count == 10){
-      obj_endpin1 = 1;
-    }
-    else{
-      count += 1;
-    }
-
+    obj_end1 = digitalRead(obj_endpin1);
+    serial_info_to_arduino.println("O" + String(-1));
+    delay(50);
   }
+
+  obj_end1 = 0;
   return obj_dist;
 }
 
-float scrn_Recallibrate(float scrn_dist) {
-  scrn_dist = 0;
-  Blynk.virtualWrite(3, 0);
+float scr_Recallibrate(float scr_dist) {
+    scr_dist = 0;
+    Blynk.virtualWrite(3, 0);
 
-  count = 0;
-  Serial.println(scrn_endpin1);
-  Serial.println('a');
-  scrn_endpin1 = digitalRead(D1);
-  Serial.println(scrn_endpin1);
-  Serial.println('b');
-  while(scrn_endpin1 != 1){
-    scrn_endpin1 = digitalRead(D1);
-    Serial.println('c');
-    Serial.println(scrn_endpin1);
-    serial_info_to_arduino.println("S"+ String(-1)); // missing
+    Serial.println(scr_endpin1);
+    Serial.println('a');
+    scr_end1 = digitalRead(scr_endpin1);
+    Serial.println(scr_end1);
+    Serial.println('b');
 
-  }
-  return scrn_dist;
+    while(scr_end1 != 1) {
+      scr_end1 = digitalRead(scr_endpin1);
+      Serial.println('c');
+      Serial.println(scr_end1);
+      serial_info_to_arduino.println("S"+ String(-1));
+
+    }
+    scr_end1 = 0;
+    return scr_dist;
 }
 
-void setup(){
+void setup() {
 
-  Blynk.begin(auth, ssid, pass); // missing
+  Blynk.begin(auth, ssid, pass);
 
   // initialize the serial port:
   Serial.begin(9600);
   serial_info_to_arduino.begin(4800);
 
-  pinMode(D1, INPUT);
+  pinMode(16, INPUT);
 
-  //obj_dist = obj_Recallibrate(obj_dist);
-  scrn_dist = scrn_Recallibrate(scrn_dist);
+  obj_dist = obj_Recallibrate(obj_dist);
+  scr_dist = scr_Recallibrate(scr_dist);
 
   //serial_info_to_arduino.println("O"+ String(+2));
   //serial_info_to_arduino.println("S"+ String(+2));
 }
 
-BLYNK_WRITE(V2){
+BLYNK_WRITE(V2) {
   obj_dist = param.asFloat();
 }
-BLYNK_WRITE(V3){
-  scrn_dist = param.asFloat();
+
+BLYNK_WRITE(V3) {
+  scr_dist = param.asFloat();
 }
-BLYNK_WRITE(V4){
+
+BLYNK_WRITE(V4) {
   Recall = param.asInt();
 }
+
+//BLYNK_WRITE(V5) {
+//  float focal_length = prev_obj * prev_scr / (prev_obj + prev_scr);
+//  
+//}
 
 
 // Count pin is just for testing without the setup
 
-void loop() {
-  count = 0;
+void loop()  {
   Blynk.run();
 
-  if(Recall == 1){
+  if(Recall == 1) {
     Recall = 0;
-    prev_obj =0;
-    prev_scrn = 0;
+    prev_obj = 0;
+    prev_scr = 0;
 
-    //obj_dist = obj_Recallibrate(obj_dist);
-    scrn_dist = scrn_Recallibrate(scrn_dist);
+    obj_dist = obj_Recallibrate(obj_dist);
+    scr_dist = scr_Recallibrate(scr_dist);
 
     Blynk.virtualWrite(4, 0);
 
@@ -125,19 +130,34 @@ void loop() {
     //serial_info_to_arduino.println("S"+ String(+2));
   }
 
-  obj_StepsToTake = (obj_dist-prev_obj)/distPerstep;
+//  if (obj_dist > prev_obj && obj_end2 != 0) {
+//      serial_info_to_arduino.println("O+1");
+//  }
+//
+//  if (obj_dist < prev_obj && obj_end1 != 0) {
+//      serial_info_to_arduino.println("O-1");
+//  }
+//
+//  if (scr_dist > prev_scr && scr_end2 != 0) {
+//      serial_info_to_arduino.println("S+1");
+//  }
+//
+//  if (scr_dist < prev_scr && scr_end1 != 0) {
+//      serial_info_to_arduino.println("S-1");
+//  }
 
-  //for object
-  scrn_StepsToTake = (scrn_dist-prev_scrn)/distPerstep;
+  obj_StepsToTake = (obj_dist-prev_obj)/distPerstep;
+  scr_StepsToTake = (scr_dist-prev_scr)/distPerstep;
 
   serial_info_to_arduino.println("O"+ String(obj_StepsToTake));
-  serial_info_to_arduino.println("S"+String(scrn_StepsToTake));
+  serial_info_to_arduino.println("S"+ String(scr_StepsToTake));
 
   prev_obj = obj_dist;
-  prev_scrn = scrn_dist;
+  prev_scr = scr_dist;
 
+  float focal_length = prev_obj * prev_scr / (prev_obj + prev_scr);
 
-
+  Blynk.virtualWrite(5, focal_length);`a
 
   //delay(Timetorotate);
 }
