fd2db36ef93018a6891319393ca9404973567e18
Gokulraj-R-002 <gokulraj18921@gmail.com>
2021-10-31 23:20:58 +0530

    a few changes made

--- nodemcu.ino
+++ nodemcu.ino
@@ -4,20 +4,18 @@
 
 #define BLYNK_PRINT Serial
 
-SoftwareSerial serial_info_to_arduino(D2, D3); // Rx and Tx pins
+SoftwareSerial serial_info_to_arduino(D2, D3); // for serial communication with arduino
+// D2 is used as Rx pin and D3 is used as Tx pin
 
-// You should get Auth Token in the Blynk App.
-// Go to the Project Settings (nut icon).
+// Blynk authorization token
 char auth[] = "arbTMNFpk18iGtb0e2SmJosJLJ41uagS";
 
-// Your WiFi credentials.
-// Set password to "" for open networks.
+// WiFi credentials
 char ssid[] = "Galaxy M31EA2F";
 char pass[] = "12344321";
 
-const int stepsPerRevolution = 200; // change this to fit the number of steps per revolution for your motor
-const float distPerstep = 0.1; //(in cm)
-const int motor_spped = 20;
+const int stepsPerRevolution = 200; // steps per revolution for the motors
+const float distPerstep = 0.02; //(in cm)
 
 int Recall = 0;
 bool obj_recal = false;
@@ -30,20 +28,19 @@ int obj_end1 = 0, obj_end2 = 0, scr_end1 = 0, scr_end2 = 0;
 float obj_dist;
 float scr_dist;
 
-float prev_obj = 0;
-float prev_scr = 0;
+float prev_obj = 9.5;
+float prev_scr = 9.5;
 int obj_StepsToTake = 0, scr_StepsToTake = 0 ;
-int Timetorotate;
 
 
 float obj_Recallibrate(float obj_dist) {
-  obj_dist = 0;
-  Blynk.virtualWrite(2, 0);
+  obj_dist = 9.5;
+  Blynk.virtualWrite(2, 9.5);
+    Serial.print(obj_endpin1);
 
   obj_end1 = digitalRead(obj_endpin1);
-  
+
   while(obj_end1 != 1) {
-    Serial.print(obj_endpin1);
     Serial.println(obj_end1);
 
     obj_end1 = digitalRead(obj_endpin1);
@@ -56,14 +53,12 @@ float obj_Recallibrate(float obj_dist) {
 }
 
 float scr_Recallibrate(float scr_dist) {
-    scr_dist = 0;
-    Blynk.virtualWrite(3, 0);
+    scr_dist = 9.5;
+    Blynk.virtualWrite(3, 9.5);
 
     Serial.println(scr_endpin1);
-    Serial.println('a');
     scr_end1 = digitalRead(scr_endpin1);
     Serial.println(scr_end1);
-    Serial.println('b');
 
     while(scr_end1 != 1) {
       scr_end1 = digitalRead(scr_endpin1);
@@ -78,19 +73,17 @@ float scr_Recallibrate(float scr_dist) {
 
 void setup() {
 
-  Blynk.begin(auth, ssid, pass);
+  Blynk.begin(auth, ssid, pass); // To make the connection with Blynk
 
-  // initialize the serial port:
   Serial.begin(9600);
-  serial_info_to_arduino.begin(4800);
+  serial_info_to_arduino.begin(4800); // for serial communication
 
   pinMode(16, INPUT);
 
+  // for recallibration at the start of the setup
   obj_dist = obj_Recallibrate(obj_dist);
   scr_dist = scr_Recallibrate(scr_dist);
 
-  //serial_info_to_arduino.println("O"+ String(+2));
-  //serial_info_to_arduino.println("S"+ String(+2));
 }
 
 BLYNK_WRITE(V2) {
@@ -105,17 +98,11 @@ BLYNK_WRITE(V4) {
   Recall = param.asInt();
 }
 
-//BLYNK_WRITE(V5) {
-//  float focal_length = prev_obj * prev_scr / (prev_obj + prev_scr);
-//  
-//}
-
-
-// Count pin is just for testing without the setup
 
 void loop()  {
-  Blynk.run();
+  Blynk.run(); // to get the info from Blynk
 
+  // if Recall is 1, do recalibration
   if(Recall == 1) {
     Recall = 0;
     prev_obj = 0;
@@ -125,39 +112,20 @@ void loop()  {
     scr_dist = scr_Recallibrate(scr_dist);
 
     Blynk.virtualWrite(4, 0);
-
-    //serial_info_to_arduino.println("O"+ String(+2));
-    //serial_info_to_arduino.println("S"+ String(+2));
   }
 
-//  if (obj_dist > prev_obj && obj_end2 != 0) {
-//      serial_info_to_arduino.println("O+1");
-//  }
-//
-//  if (obj_dist < prev_obj && obj_end1 != 0) {
-//      serial_info_to_arduino.println("O-1");
-//  }
-//
-//  if (scr_dist > prev_scr && scr_end2 != 0) {
-//      serial_info_to_arduino.println("S+1");
-//  }
-//
-//  if (scr_dist < prev_scr && scr_end1 != 0) {
-//      serial_info_to_arduino.println("S-1");
-//  }
-
+  // calculate the number of steps to take
   obj_StepsToTake = (obj_dist-prev_obj)/distPerstep;
   scr_StepsToTake = (scr_dist-prev_scr)/distPerstep;
 
+  // serially communicate the required movement information with the arduino
   serial_info_to_arduino.println("O"+ String(obj_StepsToTake));
   serial_info_to_arduino.println("S"+ String(scr_StepsToTake));
 
   prev_obj = obj_dist;
   prev_scr = scr_dist;
 
+  // calculate the focal length and write it to the dashboard
   float focal_length = prev_obj * prev_scr / (prev_obj + prev_scr);
-
-  Blynk.virtualWrite(5, focal_length);`a
-
-  //delay(Timetorotate);
+  Blynk.virtualWrite(5, focal_length);
 }
